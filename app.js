e4f0e958e23395ea60c993193db778ea97fa89fd
samreensaba <samren_saba@yahoo.com>
2024-12-08 19:10:26 +0000

    final commit

--- app.js
+++ app.js
@@ -21,13 +21,52 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/profile", isLoggenIn, (req, res) => {
-  res.send("Hi");
+app.get("/profile", isLoggenIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+  res.render("profile", { user });
+});
+
+app.get("/like/:id", isLoggenIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  if (post.likes.indexOf(req.user.userId)=== -1){
+    post.likes.push(req.user.userId);
+  }
+  else{
+    post.likes.splice(post.likes.indexOf(req.user.userId), 1);
+  }
+  console.log(post.likes)
+  await post.save()
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggenIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  
+  res.render("edit", {post});
+});
+
+app.post("/update/:id", isLoggenIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({ _id: req.params.id }, {content: req.body.content});
+
+  res.redirect("/profile");
+});
+
+app.post("/post", isLoggenIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  });
+  user.posts.push(post._id);
+  await user.save();
+
+  res.redirect("/profile");
 });
 
 app.get("/logout", (req, res) => {
-  res.cookie("token", "")
-  res.redirect("/login")
+  res.cookie("token", "");
+  res.redirect("/login");
 });
 
 app.post("/register", async (req, res) => {
@@ -52,26 +91,26 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  let {email, password } = req.body;
+  let { email, password } = req.body;
   let user = await userModel.findOne({ email });
   if (!user) return res.send("Email or password is incorrect");
 
-  bcrypt.compare(password, user.password, (err, result)=>{
-    if (result) { 
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
       let token = jwt.sign({ email, userId: user._id }, "shhhhhhh");
       res.cookie("token", token);
-      res.status(200).send("You are logged in");
-    }
-    else return res.redirect("/login");
-    
-  })
+      res.status(200).redirect("/profile");
+    } else return res.redirect("/login");
+  });
 });
 
-function isLoggenIn(req, res, next){
-  if(req.cookies.token === "") res.redirect("/login") 
-    next()
-  let data = jwt.verify(req.cookies.token, "shhhhhhh");
-  req.user = data
+function isLoggenIn(req, res, next) {
+  if (req.cookies.token === "") res.redirect("/login");
+  else {
+    let data = jwt.verify(req.cookies.token, "shhhhhhh");
+    req.user = data;
+    next();
+  }
 }
 
 app.listen(3000);
